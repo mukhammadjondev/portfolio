5860b470ec62cd707a9a307e269ec3c81994f9dd
mukhammadjondev <muhsdev@gmail.com>
2023-09-20 20:09:29 +0500

    all other sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,12 +28,53 @@ const blurHeader = () => {
 window.addEventListener('scroll', blurHeader)
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.querySelector('.contact-form'),
+      contactMessage = document.querySelector('.contact-message')
 
+const sendEmail = e => {
+  e.preventDefault()
+
+  emailjs.sendForm('service_14qzlgw', 'template_6bmigmp', '#contact-form', '8MzHebo2K5mrQrvrd')
+        .then(() => {
+          contactMessage.textContent = 'Message sent successfully ✅'
+
+          setTimeout(() => {
+            contactMessage.textContent = ''
+          }, 3000)
+          contactForm.reset()
+        }, () => {
+          contactMessage.textContent = 'Message not sent (service error) ❌'
+        })
+}
+
+contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SHOW SCROLL UP ===============*/
-
+const scrollUp = () => {
+  const scrollUp = document.getElementById('scrollup')
+  this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
 
+const scrollActive = () => {
+  const scrollY = window.pageYOffset
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight,
+          sectionTop = current.offsetTop - 58,
+          sectionId = current.getAttribute('id'),
+          sectionClass = document.querySelector(`.nav-menu a[href*='${sectionId}']`)
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionClass.classList.add('active-link')
+    } else {
+      sectionClass.classList.remove('active-link')
+    }
+  })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
